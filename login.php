7fe2e8ccf88be3b56d709f0382d5ff447ba59f15
s0m3basti <bastian@7sprojekt.de>
2020-05-24 12:41:13 +0200

    Added a login system

--- login.php
+++ login.php
@@ -15,9 +15,53 @@
             ?>
         <div class="bg">
             <div id="Inhalt" class="login">
+
+                <?php
+                    session_start();
+                    require("Datenbank/writer.php");
+
+                    echo("Das ist der HOST: $host </br>");
+
+                    if(isset($_GET['login'])){
+                        $bn = $_POST['bn'];
+                        $pw = $_POST['pw'];
+
+                        try{
+                            $db = new PDO("$host; $name" ,$user,$pass);
+                            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                            $statement = $db->prepare("SELECT * FROM login WHERE user_name = :bn");
+                            $result = $statement->execute(array('user_name' => $bn));
+                            $user = $statement->fetch();
+                        }
+                        catch(PDOException $e){
+                            $fehler = $e->getMessage();
+                            echo $fehler;
+                        }
+                        finally{
+                            $db = null;
+                        }
+
+                        if($user != false && password_verify($pw,$user['password'])){
+                            $_SESSION['userid'] = $user['id'];
+                            //weiterleiten
+                            $errorMessage = "angemeldet";
+                        }
+                        else{
+                            $errorMessage = "Benutzername oder Passwort war falsch. Bitte versuchen sie es erneut";
+                        }
+                    }
+                    
+                    
+                    //$pw = "test";
+                    //$hash = password_hash($pw, PASSWORD_DEFAULT);
+                    //echo $hash
+
+                ?>
+
                 <h1>Admin-Login</h1>
                 <p>Bitte gib deinen Benutzernamen und dein Passwort ein</p><br>
-                <form name="login" method="POST" action="admin/index.php">
+                <form name="login" method="POST" action="login.php?login=1">
                     <table id="table_login">
                         <tr>
                             <td><label>Benutzernamen </td>
@@ -29,6 +73,9 @@
                         </tr>
                         <tr>
                             <td scope="colgroup" colspan="2" style="text-align:center"><input type="submit" name="submit" value="Anmelden"></td>
+                        </tr>
+                        <tr>
+                            <td scope="colgroup" colspan="2" style="text-align:center"><p><?php echo $errorMessage ?></p></td>
                         </tr>
                     </table>
                 </form> 
