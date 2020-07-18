d198df2a36a76931ed151cbececd9acdaae13d86
s0m3basti <bastian@7sprojekt.de>
2020-07-18 22:10:26 +0200

    added an create SQL file

--- anmeldung/senden.php
+++ anmeldung/senden.php
@@ -16,7 +16,7 @@
         <link rel="stylesheet" href="../CSS/online.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
-        <script type="text/javascript" src="http://lagertest.de/files/active.js"></script>
+        <script type="text/javascript" src="<?php echo(linkmaker("/files/active.js")) ?>"></script>
         <!--<script defer type="text/javascript"> 
             function countDown(init){
                 if (init || --document.getElementById( "counter" ).firstChild.nodeValue > 0 )
