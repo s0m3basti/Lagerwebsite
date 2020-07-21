5c912185377126ec1d87456f0b533f0bf6a92681
s0m3basti <bastian@7sprojekt.de>
2020-07-21 18:47:58 +0200

    starting with the status bar

---
+++ admin/files/indexstatus.js
@@ -0,0 +1,10 @@
+switch(status){
+    case 1:
+        document.getElementById('div_status').style.backgroundColor = "#4CAF50";
+        break;
+    case 2:
+        document.getElementById('div_status').style.backgroundColor = "#ffc02b";
+        break;
+    case 3:
+        document.getElementById('div_status').style.backgroundColor = "#f44336";
+}   
--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,28 @@ if(!isset($_SESSION['userid'])) {
  
 //Abfrage der Nutzer ID vom Login
 $userid = $_SESSION['userid'];
- 
+$uvorname = $_SESSION['vorname'];
+$unachname = $_SESSION['nachname'];
+$umail = $_SESSION['mail'];
+$urechte = $_SESSION['rechte'];
+
+require "../files/linkmaker.php";
+require "../files/datenzugriff.php";
+
+switch($status){
+    case "anmeldung":
+        $status = 1;
+        break;
+    case "voranmeldung":
+        $status = 2;
+        break;
+    case "keine_anmeldung":
+        $status = 3;
+        break;
+}
+
+
+
 ?>
 
 <!DOCTYPE HTML>
@@ -15,23 +36,28 @@ $userid = $_SESSION['userid'];
     <meta charset="UTF-8">
     <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico" id="favicon">
     <link rel="stylesheet" href="CSS/admin.css">
+    <script>
+        const status = <?php echo $status ?>
+    </script>
+    <script src="files/indexstatus.js" defer></script>
 </head>
 <body>
     <?php
         require("files/nav.html");
     ?>
-    <!--
-    <div class="userdaten">
-        <p>Hallo <?php echo ($uvorname." ".$unachname) ?></p>
-    </div>
-    -->
+    <div class="content">
+        <!--<div class="userdaten">
+            <p>Hallo <?php echo ($uvorname." ".$unachname) ?></p>
+        </div>-->
+        <h1>Übersicht</h1>
+        <h2>Hier siehst du den Momentanen stand der Anmeldung.</h2>
+        <div class="status" id="div_status">
+            Hier steht der Status des Momentanen standes
+        </div>
 
-    <div>
-        <!-- Statud der Anmeldung goes here -->
-    </div>
-
-    <div>
-        <!-- Tabelle mit den Stammdaten goes here -->
+        <div>
+            <!-- Tabelle mit den Stammdaten goes here -->
+        </div>
     </div>
 </body>
 </html>
