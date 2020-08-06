f4f23468e5ae5b80febd18e356559297e1cf534d
s0m3basti <bastian@7sprojekt.de>
2020-08-06 14:16:55 +0200

    first update working

--- admin/files/avwsenden.php
+++ admin/files/avwsenden.php
@@ -6,6 +6,8 @@
     }
 +
     require "../../files/linkmaker.php";
+    require "../../files/datenzugriff.php";
+    require "../../Datenbank/writer.php";
     $id = $_GET['id'];
 
     function changetest($old, $new){
@@ -25,13 +27,49 @@
     }
     switch($_GET["type"]){
         case 1:
-            echo changetest($_SESSION['k_vorname'], $_POST["vorname"]);
-            echo changetest($_SESSION["k_nachname"], $_POST["nachname"]);
-            echo changetest($_SESSION["geschlecht"], $_POST["geschlecht"]);
-            echo changetest($_SESSION["gebdatum"], $_POST["gebdatum"]);
-            //beim Geburtstag dann auch das neue lageralter berechenn
+            if(changetest($_SESSION['k_vorname'], $_POST["vorname"]) || changetest($_SESSION["k_nachname"], $_POST["nachname"]) || changetest($_SESSION["geschlecht"], $_POST["geschlecht"]) || changetest($_SESSION["gebdatum"], $_POST["gebdatum"])){
+                    
+    
+                $anfang = strtotime($anfang);
+                $anfang = date("Y-m-d", $anfang);
+    
+                $lageralter = $anfang-$_POST["gebdatum"];
+                
+                try{
+                    $db = new PDO("$host; $name" ,$user,$pass);
+                    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+                    $sql = 'UPDATE tbl_stammdaten SET Nachname = "'.$_POST["vorname"].'", Vorname = "'.$_POST["nachname"].'", Geschlecht = "'.$_POST["geschlecht"].'", Geburtstag = "'.$_POST["gebdatum"].'", LagerAlter = "'.$lageralter.'" WHERE TeilnehmerID = "'.$id.'";';
+                                    
+                    $stmt = $db->prepare($sql);
+                    $stmt->execute();
+
+                    header("Location:verwalten.php?id=$id&message=succsess");
+                }
+                catch(PDOException $e){
+                    $fehler = $e->getMessage();
+                    echo "Es ist ein Fehler bei der Kommunikation mit der Datenbank aufgetreten. </br> $fehler";
+                    header("Location:verwalten.php?id=$id&message=fail");
+                }
+                finally{
+                    $db = null;
+                }
+            }            
             break;
         case 2:
+            if($mitglied === "on"){
+                $mitglied = "ja";
+            }
+            else{
+                $mitglied = "nein";
+            }
+
+            if($mitarbeiter === "on"){
+                $mitarbeiter = "ja";
+            }
+            else{
+                $mitarbeiter = "nein";
+            }
             break;
         case 3:
             break;
