3ecdbb51c45fd857f6a1522af64af200f7f02601
s0m3basti <bastian@7sprojekt.de>
2020-08-07 19:34:29 +0200

    added editing the new data

--- admin/files/status_voranmeldung.php
+++ admin/files/status_voranmeldung.php
@@ -1,7 +1,30 @@
 <h2> Die Voranmeldung ist zur Zeit aktiviert </h2>
 <p> Du kannst die Anmeldung jetzt aktivieren. </p>
 <p> Wenn du die Anmeldung jetzt aktivierst musst du die Daten für das nächste Jahr überprüfen, danach wird die Anmeldung freigegeben .
-    <br> Die Daten der Anmeldungen aus dem letzten Jahr werden gelöscht und die Voranmelder werden via Mail informiert.
-    <br>
-    <a href = "status_senden?von=voranmeldung&nach=anmeldung"><button>Anmeldung aktivieren</button></a>
+    <?php
+        if(!isset($_GET["step"])){
+            echo '
+                <br> Die Daten der Anmeldungen aus dem letzten Jahr werden gelöscht und die Voranmelder werden via Mail informiert.
+                <br>
+                <a href = "?step=1"><button>Anmeldung aktivieren</button></a>
+            ';
+        }
+        else{
+            echo '
+                <br> Gleiche hier die Daten für das nächste Jahr ab.
+                <br> Sind sie falsch gebe die Daten für das nächste Jahr ein.
+                <form action="files/status_senden.php?von=voranmeldung&nach=anmeldung" method="POST">
+                <table style="margin: auto">
+                    <tr> <td>Jahr des nächsten Lagers</td><td><input type="text" value='.$jahr.'></td></tr>
+                    <tr> <td>Anfang des nächsten Lagers</td><td><input type="text" value='.$anfang.'></td></tr>
+                    <tr> <td>Ende des nächsten Lagers</td><td><input type="text" value='.$ende.'></td></tr>
+                    <tr> <td colspan="2">Wenn die Daten nun stimmen klicke auf bestätigen.</td></tr>
+                    <tr> <td colspan="2"><input type="submit" value="Bestätigen"></td></tr>
+                </table>
+                </form>
+                <a href="status.php"><button>Zurück</button></a>
+            ';
+        }
+    ?>
+
 </p>
