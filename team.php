da6a401b7715617098baa26897e3b8956978feeb
s0m3basti <bastian@7sprojekt.de>
2020-08-12 17:14:20 +0200

    added steckbriefe

--- team.php
+++ team.php
@@ -20,7 +20,93 @@
         ?>
         <div class="bg">       
             <div id="Inhalt">
-                
+                <!-- <p>
+                    Work in Progress
+                </p>
+                <img src="img/work-in-progress.png" alt="Baustellenschild'Work in Progress'" title="Work in Progress" class="work"> -->
+
+                <h1>Steckbriefer der Teammitglieder</h1>
+                <br>
+                <p class="vorwort">
+                    Hier einen Text einfügen
+                </p>
+                <hr class ="steckbrief">
+
+                <?php
+                    error_reporting(0);
+
+                    $folder = "files/team/";
+                    $files = glob($folder."*.txt");
+                    $number = count($files);
+
+                    for($i = 0; $i <  $number; $i++){
+                        $text = fopen("$files[$i]","r");
+                        $name = fgets($text);
+                        $gebdatum = fgets($text);
+                        $ort = fgets($text);
+                        $rolle = fgets($text);
+                        $motivation = fgets($text);
+                        $erlebnis = fgets($text);
+                        $warum = fgets($text);
+                        $img = fgets($text);
+                        fclose($text);
+
+                        $gebdatum = strtotime($gebdatum);
+                        $gebdatum = date("Y-m-d", $gebdatum);
+                        $heute = date("Y-m-d");
+                        $alter = $heute - $gebdatum;
+                        if($alter == 0 ){
+                            $alter = "encrypted";
+                        }
+
+                    
+                        $img = "files/team/img/$img";
+                        if(!file_exists($img)){
+                            $img = "files/team/img/standart.png";
+                        }
+
+                        echo '
+                            <div class="all">
+                            <div class = "steckbrief">
+                                <table class = "steckbrief">
+                                    <tr class = "steckbrief">
+                                        <td class ="steckbrief links">Name:</td>
+                                        <td class ="steckbrief rechts">'.$name.'</td>
+                                    </tr>
+                                    <tr class = "steckbrief">
+                                        <td class ="steckbrief links">Alter:</td>
+                                        <td class ="steckbrief rechts">'.$alter.'</td>
+                                    </tr>
+                                    <tr class = "steckbrief">
+                                        <td class ="steckbrief links">Ort:</td>
+                                        <td class ="steckbrief rechts">'.$ort.'</td>
+                                    </tr>
+                                    <tr class = "steckbrief">
+                                        <td class ="steckbrief links">Rolle:</td>
+                                        <td class ="steckbrief rechts">'.$rolle.'</td>
+                                    </tr>
+                                    <tr class = "steckbrief">
+                                        <td class ="steckbrief links">Motivation:</td>
+                                        <td class ="steckbrief rechts">'.$motivation.'</td>
+                                    </tr>
+                                    <tr class = "steckbrief">
+                                        <td class ="steckbrief links">Bestes Erlebnis:</td>
+                                        <td class ="steckbrief rechts">'.$erlebnis.'</td>
+                                    </tr>
+                                    <tr class = "steckbrief">
+                                        <td class ="steckbrief links">Warum hier?</td>
+                                        <td class ="steckbrief rechts">'.$warum.'</td>
+                                    </tr>
+                                </table>
+                                </div>
+                                <div class="img">
+                                    <img src='.$img.' alt="Bild des Teammitglieds" class="steckbrief">
+                                </div>
+                                </div>
+                                <hr class ="steckbrief">
+                        ';
+                    }
+                ?>
             </div>
         </div>
          <!-- Footer einfügen -->
