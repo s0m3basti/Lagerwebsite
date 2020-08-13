75027345d7008452d09bbe705bda649cb739e5d1
s0m3basti <bastian@7sprojekt.de>
2020-08-13 21:18:12 +0200

    added a new libary and tested

--- admin/files/ausgabe_1.php
+++ admin/files/ausgabe_1.php
@@ -14,45 +14,162 @@
     require '../../files/linkmaker.php';
     require '../../files/datenzugriff.php';
     require '../../Datenbank/writer.php';
-    require "fpdf/fpdf.php";
+    require "tcpdf/tcpdf.php";
 
-    class PDF extends FPDF{
-        function header(){
-            $this->SetFont('Arial','BU',16);
-            $this->Cell(0,0,utf8_decode("Teilnehmerübersicht "),0,0,'C');
-            $this->Ln(20);
+    class MYPDF extends TCPDF {
+
+        public function Header() {
+    
+          $this->SetFont('', 'C', 50);
+          $this->SetTextColor(209,183,49);
+          $this->Ln(5);        
+          $this->Cell(0, 0, '', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+    
         }
-        function footer(){
+        public function Footer() {
             $this->SetY(-15);
-            $this->SetFont('Arial','I',8);
-            $this->Cell(0,10,'DRK Sommercamp - Erstellt am: '.date("d.m.Y"),0,0,'L');
-            $this->Cell(0,10,$this->PageNo().'/{nb}',0,0,'R');
-        }
-        function tableHead(){
-            $this->SetFont('Arial', 'B',12);
-            $this->Cell(20,10,'Vorname',1,0,'C');
-            $this->Cell(20,10,'Nachname',1,0,'C');
-            $this->Cell(20,10,'Geschlecht',1,0,'C');
-            $this->Cell(20,10,'Geburtstag',1,0,'C');
-            $this->Cell(20,10,'Alter im Lager',1,0,'C');
-            $this->Cell(20,10,'Schwimmer',1,0,'C');
-            $this->Cell(20,10,'Badeerlaubnis',1,0,'C');
-            $this->Cell(20,10,'Taschengeld',1,0,'C');
-            $this->Cell(20,10,'Art der Versicherung',1,0,'C');
-            $this->Cell(20,10,'Name der Versicherung',1,0,'C');
-            $this->Cell(20,10,'Ratenzahlung',1,0,'C');
-            $this->Cell(20,10,'Shirts',1,0,'C');
+           $this->SetFont('helvetica', 'I', 10);
+          $this->Cell(0, 0, '', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+    
         }
     }
 
-    $pdf = new PDF();
-    $pdf->AliasNbPages();
-    $pdf->SetCreator("DRK-Sommercamp");
-    $pdf->SetTitle("Teilnehmerübersicht",true);
-    $pdf->AddPage('L','A4',0,);
-    $pdf->SetFont('Arial','',12);
-    $pdf->tableHead();
-    $pdf->Output('F','doc/übersicht.pdf');
+    $header = '
+        <h1 style="font-size: 14pt; text-decoration: underline; text-align: center;">
+            Teilnehmerübersicht - '.$jahr.'
+        </h1>
+    ';
+    $footer = '
+        <br><br>
+        <hr>
+        <table style="width: 100%; font-size: 8pt">
+            <tr>
+                <td style="text-align: left; padding-top: 4px;">DRK-Sommercamp '.$jahr.'</td>
+                <td style="text-align: right; padding-top: 4px;">Erstellt am: '.date('d.m.Y').'</td>
+            </tr>
+        </table>
+    ';
+    $table_head = '
+        
+    ';
+
+    $html = nl2br(trim($header)).'
+            <table style="width: 100%; font-size: 10pt; border: solid 1px black; padding: 2px; margin-top: 500px;">
+            <tr style="border: solid 1px black; text-align: center; background-color: lightgrey;">
+                <td style="border: solid 1px black;"></td>
+                <td style="border: solid 1px black;">Vorname</td>
+                <td style="border: solid 1px black;">Nachname</td>
+                <td style="border: solid 1px black;">Geschlecht</td>
+                <td style="border: solid 1px black;">Geburtstag</td>
+                <td style="border: solid 1px black;">Alter im Lager</td>
+            </tr>';
+            try{
+                $db = new PDO("$host; $name" ,$user,$pass);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+                $i = 0;
+                $sql = "SELECT * 
+                        FROM tbl_stammdaten 
+                        WHERE Jahr = $jahr 
+                        ORDER BY Geschlecht, LagerAlter, Nachname, Vorname DESC;";
+                foreach ($db->query($sql) as $row){
+                    $i++;
+                    $html.= '<tr style="border: solid 1px black;">
+                        <td style="border: solid 1px black; text-align:right;">'.$i.'</td>
+                        <td style="border: solid 1px black; text-align:right;">'.$row['Nachname'].'</td>
+                        <td style="border: solid 1px black; text-align:right;">'.$row['Vorname'].'</td>
+                        <td style="border: solid 1px black; text-align:right;">'.$row['Geschlecht'].'</td>
+                        <td style="border: solid 1px black; text-align:right;">'.date('d.m.Y',strtotime($row['Geburtstag'])).'</td>
+                        <td style="border: solid 1px black; text-align:right;">'.$row['LagerAlter'].'</td>
+                    </tr>';
+                };
+                echo "</table>";
+            }
+            catch(PDOException $e){
+                $fehler = $e->getMessage();
+                echo "Es ist ein Fehler bei der Kommunikation mit der Datenbank aufgetreten. </br> $fehler";
+            }
+            finally{
+                $db = null;
+            }
+        
+    $html .= 
+        '</table>
+        '.$footer;
+
+
+$pdf = new MYPDF("L", "mm", "A4", true, 'UTF-8', false);
+ 
+$pdf->SetCreator("DRK Sommercamp");
+$pdf->SetAuthor("DRK Sommercamp");
+$pdf->SetTitle('Teilnehmerübersicht');
+$pdf->SetSubject('Teilnehmerübersicht');
+ 
+$pdf->SetAutoPageBreak(TRUE);
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+ 
+$pdf->SetFont('dejavusans', '', 12);
+$pdf->SetMargins(5, 0, 5, true);
+
+$pdf->AddPage();
+ 
+$pdf->writeHTML($html, true, false, true, false, '');
+ 
+$pdf->Output(dirname(__FILE__)."/doc/übersicht.pdf", 'F');
+  
+
+    // $header = '
+    //     <hr>
+    //         <h1 style="font-size: 16pt; text-decoration: underline;">
+    //             Teilnehmerübersicht - '.$jahr.'
+    //         </h2>
+    //     <hr>
+    // ';
+
+    // $footer = '
+    //     <hr>
+    //         <table style="width: 100%; bottom: 0">
+    //             <tr>
+    //                 <td style="text-align: left;">DRK-Sommercamp '.$jahr.'</td>
+    //                 <td style="text-align: right">Erstellt am: '.date('d.m.Y').'</td>
+    //             </tr>
+    //         </table>
+    // ';
+
+    // $html= '
+    //     <table>
+    //         <tr>
+    //             <td>'.nl2br(trim($header)).'</td>
+    //         </tr>
+    //         <tr>
+    //             Content goes here
+    //         </tr>
+    //     </table>
+    //     '.nl2br(trim($footer)).'
+    // ';
+
+    // $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    // $pdf->setCreator("DRK Sommercamp");
+    // $pdf->setTitle("Teilnehmerübersicht",true);
+    // $pdf->setSubject("Teilnehmerübersicht",true);
+
+    // $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+    // $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+    // $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+    // $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+    // $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    // $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+    // $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+    // $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+    // $pdf->SetFont('dejavusans ', '', 12);
+
+    // $pdf->AddPage();
+    // $pdf->writeHTML($html, true, false, true, false, '');
+    // $pdf->Output("übersicht.pdf","I");
+    // // $pdf->Output("doc/übersicht.pdf", 'F');
 
     header("Location:../ausgabe.php?task=1");
 
