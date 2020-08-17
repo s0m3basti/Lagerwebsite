8daa9ce1e35cd79a5f3eec3fda78ad933d0eea7d
s0m3basti <bastian@7sprojekt.de>
2020-08-17 19:36:01 +0200

    redirect after cookie set works now

--- files/cookie.php
+++ files/cookie.php
@@ -7,7 +7,7 @@ if(!isset($_COOKIE['BesterKeksderWelt'])){
                     <b>Verwendung von Cookies</b>
                     <br>Um unsere Webseite für Sie optimal zu gestalten und fortlaufend verbessern zu können, verwenden wir Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu.
                     <br>Weitere Informationen zu Cookies erhalten Sie in unserer <a href="'.linkmaker("/datenschutz.php").'">Datenschutzerklärung</a>.
-                    <br><a href="?cookie"><button>Verstanden!</button></a>
+                    <br><a href="?cookie&url='.$_SERVER['REQUEST_URI'].'"><button>Verstanden!</button></a>
                 </p>
             </div>
         </div>
--- files/cookie_set.php
+++ files/cookie_set.php
@@ -1,6 +1,6 @@
 <?php
     if(isset($_GET['cookie'])){
         setcookie('BesterKeksderWelt', 'true', time() + 2628000 );
-        header("Location: /.");
+        header('Location:'.$_GET['url']);
     }
 ?>
