25d6536de40b5519a7fa9c45c2b073cdfbd5ca16
s0m3basti <bastian@7sprojekt.de>
2020-08-17 23:12:54 +0200

    added sort

--- admin/files/avw2.php
+++ admin/files/avw2.php
@@ -30,22 +30,62 @@
         $db = new PDO("$host; $name" ,$user,$pass);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $i = 0;
         echo "<table class='avw'>";
-        echo "<tr class='avw_head'><td class='avw_head avw_id'></td><td class='avw_head'>Vorname des Teilnehmers</td><td class='avw_head'>Nachname des Teilnehmers</td><td class='avw_head'>Geschlecht</td><td class='avw_head'>Geburtstag</td><td class='avw_head'>Alter im Lager</td><td class='avw_head'>Vorname des Erzb</td><td class='avw_head'>Nachname des Erzb</td><td class='avw_head'>Straße</td><td class='avw_head'>Postleitzahl</td><td class='avw_head'>Ort</td><td class='avw_head'>Telefon (privat)</td><td class='avw_head'>Telefon (Handy)</td><td class='avw_head'>Telefon (dienstl.)</td><td class='avw_head'>E-Mail-Adresse</td><td class='avw_head'>Mitglied</td><td class='avw_head'>Mitarbeiter</td><td class='avw_head'>Schwimmer</td><td class='avw_head'>Schwimmstufe</td><td class='avw_head'>Badeerlaubnis</td><td class='avw_head'>Springen ins Wasser</td><td class='avw_head'>Ernaehrung</td><td class='avw_head'>Krankheiten</td><td class='avw_head'>Medikamente</td><td class='avw_head'>Taschengeld</td><td class='avw_head'>Art der Versciherung</td><td class='avw_head'>Name der Versicherung</td><td class='avw_head'>Ptivate KFZ</td><td class='avw_head'>Ratenzahlung</td><td class='avw_head'>Tshirts</td><td class='avw_head'>Umfrage</td><td class='avw_head'>Art</td><td class='avw_head'>Datum der Anmeldung</td><td class='avw_head'>IP-Adresse der Anmeldung</td></tr>";
+        echo "<tr class='avw_head'>
+                <td class='avw_head avw_id'><a href='?sort=Geschlecht,LagerAlter,Nachname,Vorname' class='index_head'><img src='img/back-arrow.png' height='20'></a></td>
+                <td class='avw_head'><a href='?sort=Vorname' class='index_head'>Vorname des Teilnehmers</a></td>
+                <td class='avw_head'><a href='?sort=Nachname' class='index_head'>Nachname des Teilnehmers</a></td>
+                <td class='avw_head'><a href='?sort=Geschlecht' class='index_head'>Geschlecht</td><td class='avw_head'>Geburtstag</a></td>
+                <td class='avw_head'><a href='?sort=LagerAlter' class='index_head'>Alter im Lager</a></td>
+                <td class='avw_head'><a href='?sort=e_Vorname' class='index_head'>Vorname des Erzb</a></td>
+                <td class='avw_head'><a href='?sort=e_Nachname' class='index_head'>Nachname des Erzb</a></td>
+                <td class='avw_head'><a href='?sort=Strasse' class='index_head'>Straße</a></td>
+                <td class='avw_head'><a href='?sort=PLZ' class='index_head'>Postleitzahl</a></td>
+                <td class='avw_head'><a href='?sort=Ort' class='index_head'>Ort</a></td>
+                <td class='avw_head'><a href='?sort=Tel_pri' class='index_head'>Telefon (privat)</a></td>
+                <td class='avw_head'><a href='?sort=Tel_handy' class='index_head'>Telefon (Handy)</a></td>
+                <td class='avw_head'><a href='?sort=Tel_dienstl' class='index_head'>Telefon (dienstl.)</a></td>
+                <td class='avw_head'><a href='?sort=email' class='index_head'>E-Mail-Adresse</a></td>
+                <td class='avw_head'><a href='?sort=mitglied' class='index_head'>Mitglied</a></td>
+                <td class='avw_head'><a href='?sort=mitarbeiter' class='index_head'>Mitarbeiter</a></td>
+                <td class='avw_head'><a href='?sort=Schwimmer' class='index_head'>Schwimmer</a></td>
+                <td class='avw_head'><a href='?sort=Schwimmstufe' class='index_head'>Schwimmstufe</a></td>
+                <td class='avw_head'><a href='?sort=Badeerlaubnis' class='index_head'>Badeerlaubnis</a></td>
+                <td class='avw_head'><a href='?sort=Springen' class='index_head'>Springen ins Wasser</a></td>
+                <td class='avw_head'><a href='?sort=Ernaehrung' class='index_head'>Ernaehrung</a></td>
+                <td class='avw_head'><a href='?sort=Krankheit' class='index_head'>Krankheiten</a></td>
+                <td class='avw_head'><a href='?sort=Medikamente' class='index_head'>Medikamente</a></td>
+                <td class='avw_head'><a href='?sort=Taschengeld' class='index_head'>Taschengeld</a></td>
+                <td class='avw_head'><a href='?sort=Versicherung_art' class='index_head'>Art der Versciherung</a></td>
+                <td class='avw_head'><a href='?sort=Versicherung_name' class='index_head'>Name der Versicherung</a></td>
+                <td class='avw_head'><a href='?sort=KFZ' class='index_head'>Ptivate KFZ</a></td>
+                <td class='avw_head'><a href='?sort=Ratenzahlung' class='index_head'>Ratenzahlung</a></td>
+                <td class='avw_head'><a href='?sort=Shirts' class='index_head'>Tshirts</a></td>
+                <td class='avw_head'><a href='?sort=umfrage' class='index_head'>Umfrage</a></td>
+                <td class='avw_head'><a href='?sort=art' class='index_head'>Art</a></td>
+                <td class='avw_head'><a href='?sort=datum' class='index_head'>Datum der Anmeldung</a></td>
+                <td class='avw_head'><a href='?sort=IP_Adresse' class='index_head'>IP-Adresse der Anmeldung</a></td>
+            </tr>";
+
+        if(!isset($_GET['sort'])){
+            $sort = "Geschlecht,LagerAlter,Nachname,Vorname";
+        }
+        else{
+            $sort = $_GET['sort'];
+        }    
 
         $sql = "SELECT * 
                     FROM tbl_stammdaten s,tbl_srgb e, tbl_anmeldedaten a
                     WHERE s.TeilnehmerID = e.TeilnehmerID
                     AND s.TeilnehmerID = a.TeilnehmerID
                     AND Jahr = $jahr 
-                    ORDER BY Geschlecht, nachname, Vorname;";
+                    ORDER BY $sort;";
         foreach ($db->query($sql) as $row){
 
 
             $i = $i + 1;
             echo 
-            '<tr class="avw">
+            '<tr class="avw" ondblclick="window.location=\'verwalten.php?id='.$row['TeilnehmerID'].'\'">
                 <td class="avw">'.$i.'</td>
                 <td class="avw">'.$row['Vorname'].'</td>
                 <td class="avw">'.$row['Nachname'].'</td>
@@ -81,7 +121,6 @@
                 <td class="avw">'.$row['Datum'].'</td>
                 <td class="avw">'.$row['IP_Adresse'].'</td>
             </tr>';
-        };
         };
         echo "</table>";
     }
