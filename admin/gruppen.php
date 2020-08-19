523d40da34823b7df7c087ff3f9ff6224fac869b
s0m3basti <bastian@7sprojekt.de>
2020-08-19 16:27:06 +0200

    group creation tool working

--- admin/gruppen.php
+++ admin/gruppen.php
@@ -46,7 +46,9 @@ require '../Datenbank/writer.php';
                         if(!isset($_GET['create'])){
                             echo '
                                 <h2> Es existiert noch keine Gruppeneinteilung </h2>
-                                <p> Du kannst jetzt erstmals Gruppen bilden, du kannst alle Eingaben später ändern.</p><br>
+                                <p> Du kannst jetzt erstmals Gruppen bilden, du kannst alle Eingaben später ändern.
+                                    <br><b> Alter dürfen sich nicht überschneiden! </b>
+                                </p><br>
                                 <form action="gruppen.php" method="GET">
                                     <input type="text" name="create" value="1" hidden>
                                     <table class="ausgabe_auswahl">
@@ -140,6 +142,108 @@ require '../Datenbank/writer.php';
                                 </table>
                             </form>';
                         }
+                    }
+                    else{
+                        $csv = array_map('str_getcsv', file('files/doc/gruppen.csv'));
+
+                        for($i = 0; $i<count($csv); $i++){
+                            if($csv[$i][0] == "w1"){
+                                $wstart = $i;
+                            }
+                        }
+                        $mgr = $wstart;
+                        $wgr = count($csv) - $wstart;
+
+                        echo'
+                            <h2>Es wurden bereits Gruppen erstellt</h2>
+                            <p> Du kannst die Einteilung nochmal ändern oder dir Dokumente ausgeben lassen </p>
+                        ';
+                        if(!isset($_GET['edit'])){
+                            echo '<a href="?edit&mgr='.$mgr.'&wgr='.$wgr.'"><button class="ausgabe">Jetzt bearbeiten</button></a><br>';
+                        }
+                        if(isset($_GET['edit'])){
+
+                            echo'<a href="gruppen.php"><button class="ausgabe gelb">Bearbeiten zuklappen</button></a><br><hr>';                          
+                            echo '
+                                <p> Du kannst die Anzahl der Gruppen und die Angaben ändern.
+                                    <br> Beim hinzufügen von Gruppen werden die Angaben automatisch kopiert, denk daran diese zu anzupassen.
+                                    <br> <b> Alter dürfen sich nicht überschneiden! </b>
+                                </p><br>
+                                <form action="gruppen.php" method="GET">
+                                    <input type="text" name="edit" value="1" hidden>
+                                    <table class="ausgabe_auswahl">
+                                        <tr>
+                                            <td>Anzahl an männlichen Gruppen</td>
+                                            <td><input type="number" min="2" max="5" value="'.$_GET['mgr'].'" name="mgr" required></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Anzahl an weiblichen Gruppen</td>
+                                            <td><input type="number" min="2" max="5" value="'.$_GET['wgr'].'" name="wgr" required></td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="2"><input type="submit" class="ausgabe" value="Bestätigen"></td>
+                                        </tr>
+                                    </table>
+                                </form>
+                            ';
+
+                            echo'
+                                <form action="files/gruppen_senden.php" method="POST">
+                                    <input type="text" name="edit" value="1" hidden>
+                                    <input type="text" name="mgr" value="'.$_GET["mgr"].'" hidden>
+                                    <input type="text" name="wgr" value="'.$_GET["wgr"].'" hidden>
+                                    <table class="ausgabe_auswahl">
+                                        <tr>
+                                            <td></td>
+                                            <td colspan="2">Alter</td>
+                                            <td colspan="4">Optionale Angaben</td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td>Von</td>
+                                            <td>Bis</td>
+                                            <td>Gruppennummer</td>
+                                            <td>Betreuer</td>
+                                            <td>Zelt</td>
+                                        </tr>
+                            ';
+                            for($i = 1 ; $i <= $_GET['mgr']; $i++){
+                                    echo '
+                                        <tr>
+                                            <td>Männlich '.$i.':</td>
+                                            <td><input type="number" min="7" max="16" name="m_min_'.$i.'" value="'.$csv[$i-1][1].'" required></td>
+                                            <td><input type="number" min="7" max="16" name="m_max_'.$i.'" value="'.$csv[$i-1][2].'" required></td>
+                                            <td><input type="text" name="m_name_'.$i.'" value="'.$csv[$i-1][3].'"></td>
+                                            <td><input type="text" name="m_betreuer_'.$i.'" value="'.$csv[$i-1][4].'"></td>
+                                            <td><input type="text" name="m_zelt_'.$i.'" value="'.$csv[$i-1][5].'"></td>
+                                        </tr>
+                                    ';
+                            }
+                            for($i = 1 ; $i <= $_GET['wgr']; $i++){
+                                echo '
+                                    <tr>
+                                        <td>Weiblich '.$i.':</td>
+                                        <td><input type="number" min="7" max="16" name="w_min_'.$i.'" value="'.$csv[$i+$wstart-1][1].'" required></td>
+                                        <td><input type="number" min="7" max="16" name="w_max_'.$i.'" value="'.$csv[$i+$wstart-1][2].'" required></td>
+                                        <td><input type="text" name="w_name_'.$i.'" value="'.$csv[$i+$wstart-1][3].'"></td>
+                                        <td><input type="text" name="w_betreuer_'.$i.'" value="'.$csv[$i+$wstart-1][4].'"></td>
+                                        <td><input type="text" name="w_zelt_'.$i.'" value="'.$csv[$i+$wstart-1][5].'"></td>
+                                    </tr>
+                                ';
+                            }
+                            echo '
+                                <tr>
+                                    <td colspan="6"><input type="submit" value="Gruppen erstellen" class="ausgabe"></td>
+                                </tr>
+                                </table>
+                            </form>';
+                        }
+                        echo'
+                            <hr>
+                            <h2>Dokumente ausgeben</h2>
+                            <a href=files/gruppen_ausgabe1.php><button class="ausgabe">Anmeldungsübersicht ausgeben</button></a>
+                            <a href=files/gruppen_ausgabe2.php><button class="ausgabe">Zeltlisten ausgeben</button></a>
+                        ';
                     }
 
 
