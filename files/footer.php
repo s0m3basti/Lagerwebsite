6b92b07a0fe60dc292593da4e6b9738618ff722e
s0m3basti <bastian@7sprojekt.de>
2020-08-29 16:34:39 +0200

    changed footer year

--- files/footer.php
+++ files/footer.php
@@ -3,7 +3,7 @@
                 <ul>
                     <li> <a href="<?php echo(linkmaker("/login.php"))?>">Login</a>
                     <li style="float:center; color:red"> Diese Webseite dient nur zur Entwicklung !!!</li>
-                    <li style="float:right"> <a href="https://www.instagram.com/s0m3basti/" target=_blank>&copy 2020 by Bastian Schneider</a> 
+                    <li style="float:right"> <a href="https://www.instagram.com/s0m3basti/" target=_blank>&copy <?php echo date('Y'); ?> by Bastian Schneider</a> 
                     <li style="float:right"> <a href="<?php echo(linkmaker("/impressum.php"))?>/"> Impressum </a>
                     <li style="float:right"> <a href="<?php echo(linkmaker("/datenschutz.php"))?>"> Datenschutz </a>
                     <li style="float:right"> <a href="<?php echo(linkmaker("/kontakt.php"))?>"> Kontakt </a>					
