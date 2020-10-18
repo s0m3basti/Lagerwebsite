15e3057039b4d30698b7c1acdfb35d9be4c007ce
s0m3basti <bastian@7sprojekt.de>
2020-10-18 13:47:08 +0200

    adding new ov webseite

--- files/footer.php
+++ files/footer.php
@@ -2,7 +2,7 @@
             <nav id=footer>
                 <ul>
                     <li> <a href="<?php echo(linkmaker("/login.php"))?>">Login</a>
-                    <li style="float:right"> <a href="https://www.instagram.com/somebasti/" target=_blank>&copy <?php echo date('Y'); ?> by Bastian Schneider</a> 
+                    <li style="float:right"> <a href="https://www.drk-ov-kw.de" target=_blank>&copy <?php echo date('Y'); ?> DRK OV KW</a> 
                     <li style="float:right"> <a href="<?php echo(linkmaker("/impressum.php"))?>/"> Impressum </a>
                     <li style="float:right"> <a href="<?php echo(linkmaker("/datenschutz.php"))?>"> Datenschutz </a>
                     <li style="float:right"> <a href="<?php echo(linkmaker("/kontakt.php"))?>"> Kontakt </a>					
